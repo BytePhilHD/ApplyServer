a0cb321c801360dab9427301c7c628a6954c453c
Christian Nagel <chris.imx@online.de>
2022-01-05 00:09:35 +0100

    Add additions (configurable display name and securing method) to EmailService class

--- src/main/java/de/bytephil/services/EmailService.java
+++ src/main/java/de/bytephil/services/EmailService.java
@@ -3,6 +3,7 @@ package de.bytephil.services;
 import de.bytephil.main.Main;
 import de.bytephil.utils.ServerConfiguration;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -13,12 +14,17 @@ public class EmailService {
         ServerConfiguration configuration = Main.config;
         //Get properties object
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.host", configuration.emailhost);
+        if (configuration.emailSecureMethod.equalsIgnoreCase("SSL")) {
+            props.put("mail.smtp.socketFactory.port", configuration.emailport);
+            props.put("mail.smtp.socketFactory.class",
+                    "javax.net.ssl.SSLSocketFactory");
+        } else {
+            props.put("mail.smtp.starttls.enable", "true");
+        }
+        props.put("mail.smtp.port", configuration.emailport);
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+
         //get Session
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
@@ -29,12 +35,19 @@ public class EmailService {
         //compose message
         try {
             MimeMessage message = new MimeMessage(session);
-            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+            if (configuration.emailDisplayName.isEmpty()) {
+                message.setFrom(new InternetAddress(configuration.emailuser));
+            } else {
+                message.setFrom(new InternetAddress(configuration.emailuser, configuration.emailDisplayName));
+            }
+            message.setRecipient(Message.RecipientType.TO,new InternetAddress(to));
             message.setSubject(sub);
             message.setText(msg);
             //send message
             Transport.send(message);
-        } catch (MessagingException e) {throw new RuntimeException(e);}
+        } catch (MessagingException e) {throw new RuntimeException(e);} catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
     }
     //  EmailService.send(config.emailuser, config.emailpassword, "phitho2018@gmail.com", "Hallo", "Hallo du");
